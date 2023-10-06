c6444680eabf46442a960b581a582100b74cee5c
asadhabeeb0 <asadhabeeb0@gmail.com>
2023-10-05 23:01:09 -0700

    Contact Form Updated

--- src/app.js
+++ src/app.js
@@ -23,18 +23,6 @@ app.get("/", (req,res) => {
     res.render("index");
 })
 
-
-app.post("/contact", async(req,res) => {
-    try{
-        // res.send(req.body);
-        const userData = new User(req.body);
-        await userData.save();
-        res.status(201).render("index");
-    }catch(error){
-        res.status(500).send(error);
-    }
-});
-
 app.listen(port, ()=>{
     console.log(`Server is listening at port number ${port}`);
 });
